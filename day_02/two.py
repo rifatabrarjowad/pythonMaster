3665cc1eacf00e017b1cdbb3a8c97d888c4f5e99
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-30 00:33:01 +0600

    number

--- day_02/two.py
+++ day_02/two.py
@@ -3,3 +3,11 @@ sum = 0
 for sum in range(number):
     sum += 1
     print(sum, end=' ')
+
+print('-------------')
+number = int(input('Enter a number to continue... : '))
+sum = 0
+
+for sum in range(number):
+    sum = sum + sum
+    print(sum, end=' ')
