a7a43a5ce1b86e5415f2a19ac6e03c95656bfb5e
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-30 01:18:00 +0600

    update number fibonnaki

--- day_02/two.py
+++ day_02/two.py
@@ -14,11 +14,22 @@ for sum in range(number):
     sum = sum + sum
     print(sum, end=' ')
 print('\n-------------')
-number = int(input('Enter a number to continue... : '))
-sum = -1
-num = 0
 
-while num < number:
-    sum = (sum+2)
-    num = num + 1
-    print(sum, end=' ')
+
+
+#Program to display the Fibonacci sequence up to n-th term
+number = int(input('Enter a number to continue... : '))
+num1 = 0
+num2 = 1
+count = 1
+
+print('Fibonacci sequence:')
+print(num1, end=' ')
+while count < number:
+
+    num3 = num1 + num2
+    print(num3, end=' ')
+    num1 = num2
+    num2 = num3
+    count += 1
+
