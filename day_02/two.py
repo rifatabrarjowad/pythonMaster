cd280151ac50546a51eb15df33a50daa26601b34
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-08-30 00:51:36 +0600

    update number

--- day_02/two.py
+++ day_02/two.py
@@ -1,13 +1,24 @@
+print('\n-------------')
 number = int(input('Enter a number to continue... : '))
+
 sum = 0
 for sum in range(number):
     sum += 1
     print(sum, end=' ')
 
-print('-------------')
+print('\n-------------')
 number = int(input('Enter a number to continue... : '))
 sum = 0
 
 for sum in range(number):
     sum = sum + sum
+    print(sum, end=' ')
+print('\n-------------')
+number = int(input('Enter a number to continue... : '))
+sum = -1
+num = 0
+
+while num < number:
+    sum = (sum+2)
+    num = num + 1
     print(sum, end=' ')
