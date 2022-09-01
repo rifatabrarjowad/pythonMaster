e18bf04400bc0765ec7c7fb9e5b2c4361c5ca5f7
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-09-01 22:27:38 +0600

    update funtion fibo

--- day_02/three.py
+++ day_02/three.py
@@ -1,5 +1,24 @@
 def lotsOfPrinting():
-  print("Printing Value")
+  print("-------------")
 
+
+def fibonacci(number):
+  # Program to display the Fibonacci sequence up to n-th term
+
+  num1 = 0
+  num2 = 1
+  count = 1
+
+  print('Fibonacci sequence:')
+  print(num1, end=' ')
+  while count < number:
+    num3 = num1 + num2
+    print(num3, end=' ')
+    num1 = num2
+    num2 = num3
+    count += 1
 
 lotsOfPrinting()
+
+number = int(input('Enter a number to continue... : '))
+fibonacci(number)
